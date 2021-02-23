60a1f2fd1f56917a36e348ef2885f4e78ad5c3c2
github username <jhernandez1692@gmail.com>
2021-02-23 13:41:02 -0500

    fixed discussion class

--- src/discussion.js
+++ src/discussion.js
@@ -1,6 +1,6 @@
 class Discussion {
 
-    constructor(comment, commentAttributes){
+    constructor(discussion, discussionAttributes){
 
         this.id = discussion.id
         this.title = discussionAttributes.title
@@ -15,8 +15,7 @@ class Discussion {
         return  `
         <div data-id=${this.id}>
                 
-        <h3>discussion: ${this.discussion.title}</h3>
-        <h3>comment:${this.content}</h3>
+        <h3>discussion: ${this.title}</h3>
         
         </div>
         <br><br>`;              
@@ -24,4 +23,4 @@ class Discussion {
 
 }
 
-Comment.all = []
+Discussion.all = []
