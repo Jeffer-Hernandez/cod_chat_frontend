a58a5f3690cb76dc91f7a65d98af0c5aca3b1bad
github username <jhernandez1692@gmail.com>
2021-02-24 11:42:18 -0500

    created post/delete action for comments

--- src/service.js
+++ src/service.js
@@ -4,6 +4,7 @@ class ApiService {
         this.baseUrl = `http://localhost:3000/api/v1`
     }
 
+    //read/ create
     findOrCreateDiscussion(e){
         return fetch(`${this.baseUrl}/discussions`, {
             method: "POST",
@@ -24,5 +25,32 @@ class ApiService {
                     return json     
                 })
     }
+
+    //create
+    postComment(e, discussion_id){
+        return fetch(`${this.baseUrl}/comments`, {
+                method: "POST",
+                headers:{
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                },
+                body: JSON.stringify(
+                    {
+                        comment: {
+                            content: e.target.children[3].value,
+                            discussion_id: discussion_id
+                        }
+                    }
+                )
+            })
+            .then(resp => resp.json())
+    }
+
+    //delete
+    deleteComment(e) {
+    fetch(`${this.baseUrl}/comments/${e.target.parentNode.dataset.id}`, {
+        method: "DELETE"
+    })
+}      
 
 }
