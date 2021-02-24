f5feacb05b98c3fc1fd1f3c44b033fd32ff5117f
github username <jhernandez1692@gmail.com>
2021-02-23 20:03:23 -0500

    more functionality to getDiscussion()

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ const discussionEndPoint = "http://localhost:3000/api/v1/discussions";
 const commentEndPoint = "http://localhost:3000/api/v1/comments";
 
 document.addEventListener('DOMContentLoaded', () => {
-  getDiscussions()
-  getComments()
-
+    getComments()
+    getDiscussions()
+  
   const createCommentForm = document.querySelector("#create-comment-form")
   createCommentForm.addEventListener("submit", (e) => createFormHandler(e))
 
@@ -12,32 +12,79 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function getDiscussions() {
-    let commentIds = [];
+    let discussionComments = [];
+    let discussion1Comments = [];
+    let discussion2Comments = [];
+    let discussion3Comments = [];
+    let discussion4Comments = [];
     fetch(discussionEndPoint)
     .then(res => res.json())
     .then(json => {
     // JSON data will be nested due to Serializer
         json.data.forEach(discussion => {
 
-    
+            let discussionId = discussion.id
             let numberOfCommentsPerDiscussion = discussion.relationships.comments.data.length
-
-            if (numberOfCommentsPerDiscussion > 0){
-                
-                let commentId = parseInt(discussion.relationships.comments.data[0].id)
             
+            if (discussionId == 1 && numberOfCommentsPerDiscussion > 0){
+                // console.log(discussion.attributes.comments)
+                
+                let comments = discussion.attributes.comments
+               
+                comments.forEach(function(item) {
+                // console.log(item)
 
-                commentIds.push(commentId)
+                discussion1Comments.push(item.content)
+                })
+            
+                let newDiscussion1 = new Discussion(discussion, discussion.attributes, discussion1Comments)
+                document.querySelector('#discussion-container').innerHTML += newDiscussion1.renderDiscussionCard()
+            }
+            else if (discussionId == 2 && numberOfCommentsPerDiscussion > 0){
+                // console.log(discussion.attributes.comments)
+                
+                let comments = discussion.attributes.comments
+               
+                comments.forEach(function(item) {
+                // console.log(item)
 
-                console.log(commentId)
+                discussion2Comments.push(item.content)
+                })
+            
+                let newDiscussion2 = new Discussion(discussion, discussion.attributes, discussion2Comments)
+                document.querySelector('#discussion-container').innerHTML += newDiscussion2.renderDiscussionCard()
+            }
+            else if (discussionId == 3 && numberOfCommentsPerDiscussion > 0){
+                // console.log(discussion.attributes.comments)
+                
+                let comments = discussion.attributes.comments
+               
+                comments.forEach(function(item) {
+                // console.log(item)
 
-                let newDiscussion = new Discussion(discussion, discussion.attributes, commentIds)
-                document.querySelector('#discussion-container').innerHTML += newDiscussion.render()
+                discussion3Comments.push(item.content)
+                })
+            
+                let newDiscussion3 = new Discussion(discussion, discussion.attributes, discussion3Comments)
+                document.querySelector('#discussion-container').innerHTML += newDiscussion3.renderDiscussionCard()
+            }
+            else if (discussionId == 4 && numberOfCommentsPerDiscussion > 0){
+                // console.log(discussion.attributes.comments)
+                
+                let comments = discussion.attributes.comments
+               
+                comments.forEach(function(item) {
+                // console.log(item)
+
+                discussion4Comments.push(item.content)
+                })
+            
+                let newDiscussion4 = new Discussion(discussion, discussion.attributes, discussion4Comments)
+                document.querySelector('#discussion-container').innerHTML += newDiscussion4.renderDiscussionCard()
             }
             else {
-                console.log("no comments")
-                let newDiscussion = new Discussion(discussion, discussion.attributes, commentIds)
-                document.querySelector('#discussion-container').innerHTML += newDiscussion.render()
+                let noCommentDiscussion = new Discussion(discussion, discussion.attributes, discussionComments)
+                document.querySelector('#discussion-container').innerHTML += noCommentDiscussion.renderNoDiscussionCard()
             }
         })
     })
@@ -88,7 +135,8 @@ function postFetch(content, discussion_id){
     commentData = comment.data
     // render JSON response
     let newComment = new Comment(commentData, commentData.attributes)
+    let newCommentDiscussion = new Discussion()
   
-    document.querySelector('#discussion-container').innerHTML += newComment.render()
+    document.querySelector('#discussion-container').innerHTML += newComment.renderDiscussionCard()
     })
 }
