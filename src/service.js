6cad41bfe28e17d06387d733b4cd51aec1878457
github username <jhernandez1692@gmail.com>
2021-02-24 11:37:52 -0500

    findorCreateDiscussion function created.

--- src/service.js
+++ src/service.js
@@ -4,5 +4,25 @@ class ApiService {
         this.baseUrl = `http://localhost:3000/api/v1`
     }
 
+    findOrCreateDiscussion(e){
+        return fetch(`${this.baseUrl}/discussions`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(
+                {
+                    discussion: {
+                        title: e.target.children[1].value
+                    }
+                })
+            })
+                .then(resp => {
+                    let json = resp.json()
+                    console.log(json) 
+                    return json     
+                })
+    }
 
 }
